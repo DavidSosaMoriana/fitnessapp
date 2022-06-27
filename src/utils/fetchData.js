958f5fd4250e4890b533d97e2ca6cc9ebc646c1a
DavidSosaMoriana <sosamorianadav@gmail.com>
2022-06-27 10:05:33 +0100

    Exersices and Loader

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export  const exerciseOptions = {
   }
 };
 
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '159cb5e053msh672957000ceedfcp172c5cjsn3c838498df74',
+  },
+};
+
 export const fetchData = async (url,  options) => {
   const response = await fetch (url, options)
   const data = await response.json()
